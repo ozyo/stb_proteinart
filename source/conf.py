dd08c67c8cce2f756657494b857dabc7b602a82f
ozyo <ozgeyoluk86@gmail.com>
2016-10-31 09:50:13 -0700

    fixed typo

--- source/conf.py
+++ source/conf.py
@@ -38,7 +38,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'npshinx',
+    'nbshinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
